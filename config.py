1e98947f6deeeea01d84213f9892ce5cec57bbc0
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-02-22 00:56:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 API_ID = e("API_ID", 13691707)
 API_HASH = e("API_HASH", "2a31b117896c5c7da27c74025aa602b8")
 
-BOT_TOKEN = e("BOT_TOKEN", None)
+BOT_TOKEN = e("BOT_TOKEN", "5450609038:AAHo5TJjtH5DnGD4aBoaTtE9J7nqOisX0jI")
 
 mongo = MongoClient("mongodb+srv://Cyberpunk:alpha@cluster0.mxvicxt.mongodb.net/?retryWrites=true&w=majority")
 db = mongo.CRED
